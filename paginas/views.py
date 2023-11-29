039c44c608299c59a39e1448089cf4e5068e826b
Antonio Sousa <antonioufpi@gmail.com>
2023-11-29 02:28:41 -0300

    views de paginas

--- paginas/views.py
+++ paginas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http import HttpRequest
 
 # Create your views here.
+def inicio(request: HttpRequest) -> HttpResponse:
+    return render(request, "paginas/inicio.html")

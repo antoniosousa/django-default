41c7731f65e49defbf15c854d3c2e94a3404905d
Antonio Sousa <antonioufpi@gmail.com>
2023-11-29 03:09:39 -0300

    configuração de templetas na raiz do projeto

--- portfolio_pessoal/settings.py
+++ portfolio_pessoal/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = "portfolio_pessoal.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "portfolio_pessoal/templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
